0542c101a7bbab02e0c935e71a320324d8b91be3
jasonaowu <2387021499@qq.com>
2023-02-08 22:06:01 +0800

    Site updated: 2023-02-08 22:05:49

--- js/cursor.js
+++ js/cursor.js
@@ -82,3 +82,28 @@ class Cursor {
     CURSOR = new Cursor();
     // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+// 一张图片
+'use strict';
+const cheerio = require('cheerio');
+
+/**
+ * 在页面插入新顶部图
+ * @param {cheerio.Root} $ Root
+ */
+function insertTopImg($) {
+    let header = $('#page-header');
+    if (header.length === 0) return;
+    let background = header.css('background-image');
+    if (!background) return;
+    $('#post, #page, #archive, #tag, #category').prepend(`<div class="top-img" style="background-image: ${background};"></div>`);
+}
+
+hexo.extend.filter.register('after_render:html', function (str, data) {
+    let $ = cheerio.load(str, {
+        decodeEntities: false
+    });
+    insertTopImg($);
+    return $.html();
+});
+
